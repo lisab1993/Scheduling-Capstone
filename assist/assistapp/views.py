eebf50ff20791eb71335af422cdc009ff4e09897
lisab1993 <brown.lisa999@gmail.com>
2021-03-11 13:55:42 -0800

    Saving before changing views.py

--- assist/assistapp/views.py
+++ assist/assistapp/views.py
@@ -11,7 +11,9 @@ from datetime import datetime
 # MY_EVENTS IS BEING USED AS A PLACEHOLDER AS IT'S BEING BUILT FIRST
 # STILL NEED TO ADD SECURITY SO USERS ONLY SEE THEIR OWN STUFF
 
-## Event-related views
+# Event-related views
+
+
 @login_required
 def my_events(request):
     '''Displays all events for a user'''
@@ -21,7 +23,7 @@ def my_events(request):
     # put it in the context object to render on the my_events page
     context = {
         'user_events': user_events,
-        }
+    }
     return render(request, 'assistapp/my_events.html', context)
 
 
@@ -29,6 +31,7 @@ def my_events(request):
 def show_add_event(request):
     '''Renders the template to add an event'''
     return render(request, 'assistapp/add_event.html')
+
 
 @login_required
 def add_event(request):
@@ -38,10 +41,12 @@ def add_event(request):
         start_date = request.POST['start_date']
         end_date = request.POST['end_date']
         user = request.user
-        event = Event(title=title, start_date=start_date, end_date=end_date, user=user)
+        event = Event(title=title, start_date=start_date,
+                      end_date=end_date, user=user)
         event.save()
         return redirect('assistapp:my_events')
-    
+
+
 @login_required
 def delete_event(request, pk):
     '''Delete an event and it's tasks'''
@@ -51,13 +56,15 @@ def delete_event(request, pk):
     event.delete()
     return redirect('assistapp:my_events')
 
+
 @login_required
-def show_edit_event(request,pk):
+def show_edit_event(request, pk):
     '''Renders the template to edit an event'''
     event = get_object_or_404(Event, pk=pk)
     if event.user != request.user:
         raise Http404
-    return render(request, 'assistapp/edit_events.html', {'event':event})
+    return render(request, 'assistapp/edit_events.html', {'event': event})
+
 
 @login_required
 def edit_event(request, pk):
@@ -69,24 +76,20 @@ def edit_event(request, pk):
         event.title = request.POST['title']
         event.start_date = request.POST['start_date']
         event.end_date = request.POST['end_date']
-        event.user = request.user 
+        event.user = request.user
         event.save()
     return redirect('assistapp:my_events')
 
 
-
-## EventTask-related views
+# EventTask-related views
 @login_required
 def task_list(request, event_id):
-    '''A list of tasks for the selected event(event detail page)'''
-
-
-
     '''A list of tasks for a specific event'''
     event = Event.objects.get(id=event_id)
     if event.user != request.user:
         raise Http404
     return render(request, 'assistapp/task_list.html', {'event': event})
+
 
 @login_required
 def show_add_task(request, event_id):
@@ -94,7 +97,8 @@ def show_add_task(request, event_id):
     event = Event.objects.get(id=event_id)
     if event.user != request.user:
         raise Http404
-    return render(request, 'assistapp/add_task.html', {'event':event})
+    return render(request, 'assistapp/add_task.html', {'event': event})
+
 
 @login_required
 def add_task(request, event_id):
@@ -109,17 +113,20 @@ def add_task(request, event_id):
         priority_urgency = request.GET.get('priority_urgency')
         notes = request.POST['notes']
         image = request.FILES.get('image')
-        task = EventTask(name=name, due_date=due_date, notes=notes, image=image, priority=priority_urgency, event_id=event_id)
+        task = EventTask(name=name, due_date=due_date, notes=notes,
+                         image=image, priority=priority_urgency, event_id=event_id)
         task.save()
         return HttpResponseRedirect(reverse('assistapp:task_list', args=[event_id]))
+
 
 @login_required
 def delete_task(request, pk):
     '''Deletes a task'''
     task = get_object_or_404(EventTask, pk=pk)
     task.delete()
-    #the request is going to include the page it came from
+    # the request is going to include the page it came from
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
 
 @login_required
 def show_edit_task(request, id):
@@ -130,8 +137,8 @@ def show_edit_task(request, id):
     if task.event.user != request.user:
         raise Http404
     context = {
-        'task':task,
-        'priorities':priorities
+        'task': task,
+        'priorities': priorities
     }
     return render(request, 'assistapp/edit_task.html', context)
 
@@ -147,28 +154,37 @@ def edit_task(request, id):
     task.notes = request.POST['notes']
     task.image = request.FILES.get('image')
     task.save()
-    #tell the program to return to the task list at the task's event id
+
+    if 'clear_image' in request.POST:
+        task.image = None
+    elif 'image' in request.FILES:
+        image = request.FILES['image']
+        task.image = image
+    # tell the program to return to the task list at the task's event id
     return redirect('assistapp:task_list', event_id=task.event_id)
 
     # path('<int:id>/edit_task/', views.edit_task, name="edit_task")
 
-#Calendar
+# Calendar
 
+
+@login_required
 def show_calendar(request):
     return render(request, 'assistapp/calendar.html')
 
+
+@login_required
 def get_events(request):
     user = request.user
     events = Event.objects.filter(user=user)
     events_list = []
     for event in events:
         events_list.append({
-           'event-name':event.title,
-           'start':event.start_date.strftime('%Y-%m-%d %H:%M'),
-           'end':event.end_date.strftime('%Y-%m-%d %H:%M'),
-           'color':'blue',
-           'name': event.title
+            'event-name': event.title,
+            'start': event.start_date.strftime('%Y-%m-%d %H:%M'),
+            'end': event.end_date.strftime('%Y-%m-%d %H:%M'),
+            'color': 'blue',
+            'name': event.title
         })
-    print(events_list)
-    return JsonResponse({'events':events_list})
-    
+    # print(events_list)
+    return JsonResponse({'events': events_list})
